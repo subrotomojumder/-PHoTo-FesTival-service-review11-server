31dd5a4c2873726ab4e1510ee88841b03d7a1de0
Janardhan Majumder <subrotomojumder03@gmail.com>
2022-11-07 22:43:45 +0600

    mongo db connection ok

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,19 +10,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-async function run(){
-    try{
-        app.get('/', async(req, res)=> {
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uxk5wr6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    const serviceCollection = client.db("service-review").collection("service");
+    try {
+        app.get('/', async (req, res) => {
             res.send('service review assignment running')
         })
     }
-    catch(err){
+    catch (err) {
         console.log(err.stack);
         console.log(err.message);
     }
 }
 run().catch(err => console.log(err));
 
-app.listen(port, ()=> {
-    console.log('server port :', port)
+app.listen(port, () => {
+    client.connect(err => {
+        if (err) {
+            console.log('database connection err')
+        }
+        else {
+            console.log('server port :', port)
+        }
+    });
 })
