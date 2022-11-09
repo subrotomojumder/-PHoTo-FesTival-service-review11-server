3c2141b21a8500e6abd0b35ee537764ec441f82c
Janardhan Majumder <subrotomojumder03@gmail.com>
2022-11-09 17:07:48 +0600

    get uniqe service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -19,12 +19,32 @@ async function run() {
     const serviceCollection = client.db("service-review").collection("service");
     try {
         // service create api
-        app.post('/services', async(req, res)=> {
+        app.post('/services', async (req, res) => {
             const service = req.body;
             console.log(service)
             const results = await serviceCollection.insertOne(service);
             res.send(results);
         })
+        // get services api
+        app.get('/services', async (req, res) => {
+            const limit = parseInt(req.query.limit);
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            let services;
+            if (limit) {
+                services = await cursor.limit(limit).toArray();
+            }else{
+                services = await cursor.toArray();
+            }
+            res.send(services);
+        })
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
         app.get('/', async (req, res) => {
             res.send('service review assignment running')
         })
